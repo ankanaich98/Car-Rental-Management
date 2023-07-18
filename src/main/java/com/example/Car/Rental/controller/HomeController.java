40bfb7fac68eb3fedeaf81ab6fc0b4c377551b6e
ankanaich98 <ankan98aich@gmail.com>
2023-07-18 05:23:05 -0800

    Home page added

---
+++ src/main/java/com/example/Car/Rental/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.Car.Rental.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+
+public class HomeController {
+    @RequestMapping("/")
+    public String mainPage(){
+        return "home";
+    }
+}
