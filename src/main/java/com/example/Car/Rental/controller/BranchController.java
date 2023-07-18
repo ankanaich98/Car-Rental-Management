bd321c1f20b854d27a2bb14bef4f6c098a047b01
ankanaich98 <ankan98aich@gmail.com>
2023-07-18 04:48:43 -0800

    Branch crud done

--- src/main/java/com/example/Car/Rental/controller/BranchController.java
+++ src/main/java/com/example/Car/Rental/controller/BranchController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -33,6 +34,18 @@ public class BranchController {
     @PostMapping("/branch/save")
     public String saveForm(Branch branch) {
         branchService.save(branch);
+        return "redirect:/branches";
+    }
+    @GetMapping("/branch/edit/{id}")
+    public String showEditForm(@PathVariable("id") Long id, Model model){
+            Branch branches= branchService.get(id);
+            model.addAttribute("branches", branches);
+//            model.addAttribute("addBranch","Edit Branch (ID " +id+")" );
+            return "addBranch";
+    }
+    @GetMapping("/branch/delete/{id}")
+    public String deleteUser(@PathVariable("id") Long id){
+        branchService.delete(id);
         return "redirect:/branches";
     }
 
--- src/main/java/com/example/Car/Rental/service/BranchService.java
+++ src/main/java/com/example/Car/Rental/service/BranchService.java
@@ -6,18 +6,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BranchService {
     private final BranchRepository branchRepository;
+
     @Autowired
     public BranchService(BranchRepository repository) {
         this.branchRepository = repository;
     }
-    public List<Branch> listAllBranches(){
+
+    public List<Branch> listAllBranches() {
         return (List<Branch>) branchRepository.findAll();
     }
-    public void save(Branch branch){
+
+    public void save(Branch branch) {
         branchRepository.save(branch);
+    }
+
+    public Branch get(Long id) {
+        Optional<Branch> optionalUser = branchRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            return optionalUser.get();
+        }
+        else {
+            return null;
+        }
+    }
+    public void delete(Long id){
+        branchRepository.deleteById(id);
     }
 }
