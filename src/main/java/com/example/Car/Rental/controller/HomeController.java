b52f34262b9f62928483c1c3f8aa5379692fe3c8
ankanaich98 <ankan98aich@gmail.com>
2023-07-18 23:10:38 -0800

    Dashboard added

--- src/main/java/com/example/Car/Rental/controller/HomeController.java
+++ src/main/java/com/example/Car/Rental/controller/HomeController.java
@@ -1,12 +1,14 @@
 package com.example.Car.Rental.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 
 public class HomeController {
     @RequestMapping("/")
-    public String mainPage(){
+    public String mainPage(Model model){
+        model.addAttribute("formTitle", "Dashboard");
         return "home";
     }
 }
