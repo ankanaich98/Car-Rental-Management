008f3da53017ed85c437d5b1af54fa5e19297e2f
ankanaich98 <ankan98aich@gmail.com>
2023-07-21 03:35:16 -0800

    Added success messages for branches

--- src/main/java/com/example/Car/Rental/controller/BranchController.java
+++ src/main/java/com/example/Car/Rental/controller/BranchController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -34,8 +35,10 @@ public class BranchController {
     }
 
     @PostMapping("/branch/save")
-    public String saveForm(Branch branch) {
+    public String saveForm(Branch branch, RedirectAttributes redirectAttributes) {
         branchService.save(branch);
+        String Message ="Entry created Successfully";
+        redirectAttributes.addFlashAttribute("Message",Message);
         return "redirect:/branches";
     }
     @GetMapping("/branch/edit/{id}")
@@ -46,8 +49,10 @@ public class BranchController {
             return "addBranch";
     }
     @PostMapping("/branch/delete/{id}")
-    public String deleteBranch(@PathVariable("id") Long id){
+    public String deleteBranch(@PathVariable("id") Long id,RedirectAttributes redirectAttributes){
         branchService.delete(id);
+        String Message ="Entry deleted Successfully";
+        redirectAttributes.addFlashAttribute("Message",Message);
         return "redirect:/branches";
     }
 
