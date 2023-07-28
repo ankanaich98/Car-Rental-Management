1b10e1b1434954bece7adeb0239d6526d5b3ab13
ankanaich98 <ankan98aich@gmail.com>
2023-07-28 00:43:50 -0800

    Only available cars will show for bookings and Dashboard

--- src/main/java/com/example/Car/Rental/controller/HomeController.java
+++ src/main/java/com/example/Car/Rental/controller/HomeController.java
@@ -1,14 +1,29 @@
 package com.example.Car.Rental.controller;
 
+import com.example.Car.Rental.entity.Car;
+import com.example.Car.Rental.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
 @Controller
 
 public class HomeController {
+    private final CarService carService;
+
+    @Autowired
+    public HomeController(CarService carService) {
+        this.carService = carService;
+    }
+
     @RequestMapping("/")
     public String mainPage(Model model){
         model.addAttribute("formTitle", "Dashboard");
+        List<Car> listAllCars = carService.listAllCars();
+        model.addAttribute("cars",listAllCars);
         return "home";
     }
 }
