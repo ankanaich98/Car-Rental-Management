884c3aed47060b1af9b4fb7b709e7bd66ad5ad4d
ankanaich98 <ankan98aich@gmail.com>
2023-08-02 01:24:17 -0800

    Dashboard view changed

--- src/main/java/com/example/Car/Rental/controller/CarController.java
+++ src/main/java/com/example/Car/Rental/controller/CarController.java
@@ -9,9 +9,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class CarController {
@@ -23,8 +26,14 @@ public class CarController {
     }
 
     @GetMapping("/cars")
-    public String showCarList(Model model){
-        List<Car> listAllCars = carService.listAllCars();
+    public String showCarList(@RequestParam(value = "isAvailable", required = false) boolean isAvailable, Model model){
+        List<Car> listAllCars;
+
+        if (isAvailable) {
+            listAllCars = carService.listAllCars().stream().filter(car -> car.isAvailability() == true).collect(Collectors.toList());
+        } else {
+            listAllCars = carService.listAllCars();
+        }
         model.addAttribute("cars",listAllCars);
         model.addAttribute("formTitle", "Car List");
         return "cars";
--- src/main/java/com/example/Car/Rental/controller/HomeController.java
+++ src/main/java/com/example/Car/Rental/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.example.Car.Rental.controller;
 
+import com.example.Car.Rental.entity.Branch;
 import com.example.Car.Rental.entity.Car;
 import com.example.Car.Rental.entity.Customer;
+import com.example.Car.Rental.service.BranchService;
 import com.example.Car.Rental.service.CarService;
 import com.example.Car.Rental.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,25 +12,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 
 public class HomeController {
     private final CarService carService;
     private final CustomerService customerService;
+
+    private final BranchService branchService;
     @Autowired
-    public HomeController(CarService carService, CustomerService customerService) {
+    public HomeController(CarService carService, CustomerService customerService, BranchService branchService) {
         this.carService = carService;
         this.customerService = customerService;
+        this.branchService = branchService;
     }
 
     @RequestMapping("/")
     public String mainPage(Model model){
         model.addAttribute("formTitle", "Dashboard");
         List<Car> listAllCars = carService.listAllCars();
-        model.addAttribute("cars",listAllCars);
+        model.addAttribute("cars",listAllCars.size());
+        model.addAttribute("availableCars",listAllCars.stream().filter(car -> car.isAvailability()==true).collect(Collectors.toList()).size());
         List<Customer> listAllCustomers = customerService.listAllCustomers();
-        model.addAttribute("customers",listAllCustomers);
+        model.addAttribute("customers",listAllCustomers.size());
+        List<Branch> listAllBranches = branchService.listAllBranches();
+        model.addAttribute("branches",listAllBranches.size());
         return "home";
     }
 }
