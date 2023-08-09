68a6b5b514f5b98086c7b5b03a36017b68c45298
ankanaich98 <ankan98aich@gmail.com>
2023-08-09 21:08:48 +0600

    Worked on chart-pie-demo.js to make pie charts based on revenue

--- src/main/resources/static/assets/demo/chart-pie-demo.js
+++ src/main/resources/static/assets/demo/chart-pie-demo.js
@@ -4,9 +4,29 @@ Chart.defaults.global.defaultFontColor = '#292b2c';
 
 
 
-    fetch('/pie-chart')
+    // fetch('/pie-chart')
+    // .then(response => response.json())
+    // .then(data => alert(JSON.stringify(data)));
+let fetchedJsonString;
+let  jsonArray;
+fetch('pie-chart')
     .then(response => response.json())
-    .then(data => alert(JSON.stringify(data)));
+    .then(data => JSON.stringify(data))
+    .then(jsonString => {
+      fetchedJsonString = jsonString; // Store the JSON string in the variable
+      jsonArray = JSON.parse(fetchedJsonString);
+      var ctx = document.getElementById("myPieChart");
+      var myPieChart = new Chart(ctx, {
+        type: 'pie',
+        data: {
+          labels: [jsonArray[0].name, jsonArray[1].name, jsonArray[2].name, jsonArray[3].name,jsonArray[4].name],
+          datasets: [{
+            data: [parseFloat(jsonArray[0].revenue.replace(/,/g, '')), parseFloat(jsonArray[1].revenue.replace(/,/g, '')), parseFloat(jsonArray[2].revenue.replace(/,/g, '')),parseFloat(jsonArray[3].revenue.replace(/,/g, '')),parseFloat(jsonArray[4].revenue.replace(/,/g, ''))],
+            backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745','#800080'],
+          }],
+        },
+      });
+    });
 
 /*var objectsFetch = fetch('/pie-chart').then(response => response.json()).then(data => JSON.stringify(data))
 alert(objectsFetch)*/
@@ -14,14 +34,16 @@ alert(objectsFetch)*/
 // alert(labelsPie);
 
 // Pie Chart Example
-var ctx = document.getElementById("myPieChart");
-var myPieChart = new Chart(ctx, {
-  type: 'pie',
-  data: {
-    labels: ["Blue", "Red", "Yellow", "Green","Purple"],
-    datasets: [{
-      data: [12.21, 15.58, 11.25, 8.32,10.2],
-      backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745','#800080'],
-    }],
-  },
-});
+
+
+// var ctx = document.getElementById("myPieChart");
+// var myPieChart = new Chart(ctx, {
+//   type: 'pie',
+//   data: {
+//     labels: ["Blue", "Red", "Yellow", "Green","Purple"],
+//     datasets: [{
+//       data: [12.21, 15.58, 11.25, 8.32,10.2],
+//       backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745','#800080'],
+//     }],
+//   },
+// });
